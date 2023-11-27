8d8be6ae21c1f1ea4202c9093d3ebd7a44c79340
Prajwal Singh <102660402+Singh-Prajwal@users.noreply.github.com>
2023-11-27 22:45:12 +0530

    base name added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import Coin from './components/coins'
-
+import "./App.css"
+import Coin from "./components/coins"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      <Coin/>
+      <BrowserRouter basename="/coindown">
+        <Routes>
+          <Route path="/" element={<Coin />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
